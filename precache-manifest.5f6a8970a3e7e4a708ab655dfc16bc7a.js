45737323dccb341466b5c64982ea7017c0d8c1da
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-28 19:29:32 +0530

    Updates

---
+++ precache-manifest.5f6a8970a3e7e4a708ab655dfc16bc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb40e56a5c25259afe8764844c63b18",
+    "url": "/fakeshop/index.html"
+  },
+  {
+    "revision": "81a075db8a99cb96d34f",
+    "url": "/fakeshop/static/css/main.c1126be1.chunk.css"
+  },
+  {
+    "revision": "2bd76cd4acaad3ef548c",
+    "url": "/fakeshop/static/js/2.1fccd1bc.chunk.js"
+  },
+  {
+    "revision": "81a075db8a99cb96d34f",
+    "url": "/fakeshop/static/js/main.55fd4bc0.chunk.js"
+  },
+  {
+    "revision": "70747f9bdb4e433c7410",
+    "url": "/fakeshop/static/js/runtime-main.1b09471d.js"
+  }
+]);
